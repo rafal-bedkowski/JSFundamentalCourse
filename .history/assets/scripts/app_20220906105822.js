79f1d1085b28ec2d5838ab559de9326e23e9cc6a
Rafal Bedkowski <bekowe.fara@gmail.com>
2022-09-06 12:54:00 +0200

    add small reformt to the template literal string

---
+++ .history/assets/scripts/app_20220906105822.js
@@ -0,0 +1,3 @@
+let currentResult = 0;
+
+currentResult = currentResult + 10;
---
+++ .history/assets/scripts/app_20220906115013.js
@@ -0,0 +1,8 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+let calculateDescription = '(' + defaultResult + '10 ) * 3 / 2 - 1';
+
+outputResult(currentResult, calculateDescription);
