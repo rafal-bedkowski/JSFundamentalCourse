1f15bd37ccc389d9fd36c854b891c1a5d1264f86
Rafal Bedkowski <bekowe.fara@gmail.com>
2022-09-09 11:21:07 +0200

    update on app.js

---
+++ .history/assets/scripts/app_20220909110555.js
@@ -0,0 +1,63 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+//get the input from input field
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+//generate and writes calclulation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); //from vendor.js
+}
+
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function addition() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtraction() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiplication() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divided() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+addBtn.addEventListener('click', addition);
+subtractBtn.addEventListener('click', subtraction);
+multiplyBtn.addEventListener('click', multiplication);
+divideBtn.addEventListener('click', divided);
